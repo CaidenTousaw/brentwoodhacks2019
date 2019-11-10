30c01b68b4b7d012cf4d99e1d5e35dc3333619b6
CaidenTousaw <57578640+CaidenTousaw@users.noreply.github.com>
2019-11-09 20:25:24 -0800

    forgot to save because im dumb dumb

--- Program.py
+++ Program.py
@@ -1,6 +1,8 @@
 #ASSIGNING_ALL_SCHOOL_CLASSES_100_POINTS
 
-points = 100
+points= 100
+
+#ALL GRADE 11 COURSES
 
 Pre_Calculus_11 = points
 Foundations_of_Mathematics_12 = points
@@ -19,4 +21,33 @@ Geology_12 = points
 Physics_11 = points
 AP_Physics_11 = points
 
-print(Chemistry_11_or_AP_Chemistry_11 + 10)
+#ALL GRADE 11 ELECTIVES
+
+Twentyth_Century_World_History_12 = points
+Beginner's_Mandarin_Chinese = points
+Beginner's_Spanish = points
+BC_First_Peoples_12 = points
+Chemistry_11_or_AP_Chemistry_11 = points
+Javascript = points
+Python = points
+Comparative_Cultures_12 = points
+AP_Computer_Science_Principles_12 = points
+Entrepreneurship_12 = points
+AP_Environmental_Science_12 = points
+Financial_Accounting_12 = points
+French_11 = points
+Geology_12 = points
+Human_Geography_12 = points
+Law_Studies_12 = points
+Life_Sciences_11 = points
+AP_Macroeconomics_12 = points
+Mandarin_Chinese_11 = points
+AP_Microeconomics_12 = points
+AP_Music_Theory_12 = points
+Physical_Geography_12 = points
+Physics_11_or_AP_Physics_11 = points
+Political_Studies_12 = points
+AP_Seminar_11 = points
+Social_Justice_12 = points
+Spanish_11 = points
+AP_Studio_Art_12 = points
