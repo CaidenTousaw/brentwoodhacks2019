965b9f380dfb57256a7568894085498a96c5ce2d
eamonryan123 <57578715+eamonryan123@users.noreply.github.com>
2019-11-09 18:01:09 -0800

    pasted class list - g12

--- BC_ClassList.py
+++ BC_ClassList.py
@@ -1,3 +1,37 @@
 a = ("hello", "女号")
 
 print(*a, sep = "\n") #prints them all in new lines
+# 20th Century World History 12
+# Advanced Topics in Math 12
+# Anatomy & Physiology 12 (formerly Biology 12)
+# AP Art History 12
+# AP Biology 12
+# BC First Peoples 12
+# Calculus 12 or AP Calculus AB 12
+# Chemistry 12 or AP Chemistry 12
+# Comparative Cultures 12
+# AP Comparative Government & Politics 12
+# AP Computer Science Principles 12
+# Entrepreneurship 12
+# AP Environmental Science 12
+# Financial Accounting 12
+# Foundations of Mathematics 12
+# French 12 or AP French Language & Culture 12 or DELF Level B2
+# Geology 12
+# Human Geography 12 or AP Human Geography 12
+# Law Studies 12
+# AP Macroeconomics 12
+# Mandarin Chinese 12
+# AP Microeconomics 12
+# AP Music Theory 12
+# Physical Geography 12 (formerly Geography 12)
+# Physics 12 or AP Physics 12
+# Political Studies 12
+# Pre-Calculus 12
+# AP Psychology 12
+# AP Research 12
+# Social Justice 12
+# Spanish 12 or AP Spanish Language & Culture 12 or DELE Level B2
+# AP Studio Art 12
+# AP United States History 12
+# Writing 12
