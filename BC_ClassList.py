7701669a014cbab040011bf47b7e553715bd8268
CaidenTousaw <57578640+CaidenTousaw@users.noreply.github.com>
2019-11-09 20:23:45 -0800

    added class point values

--- BC_ClassList.py
+++ BC_ClassList.py
@@ -1,37 +1,18 @@
-a = ("hello", "女号")
 
-print(*a, sep = "\n") #prints them all in new lines
-# 20th Century World History 12
-# Advanced Topics in Math 12
-# Anatomy & Physiology 12 (formerly Biology 12)
-# AP Art History 12
-# AP Biology 12
-# BC First Peoples 12
-# Calculus 12 or AP Calculus AB 12
-# Chemistry 12 or AP Chemistry 12
-# Comparative Cultures 12
-# AP Comparative Government & Politics 12
-# AP Computer Science Principles 12
-# Entrepreneurship 12
-# AP Environmental Science 12
-# Financial Accounting 12
-# Foundations of Mathematics 12
-# French 12 or AP French Language & Culture 12 or DELF Level B2
-# Geology 12
-# Human Geography 12 or AP Human Geography 12
-# Law Studies 12
-# AP Macroeconomics 12
-# Mandarin Chinese 12
-# AP Microeconomics 12
-# AP Music Theory 12
-# Physical Geography 12 (formerly Geography 12)
-# Physics 12 or AP Physics 12
-# Political Studies 12
-# Pre-Calculus 12
-# AP Psychology 12
-# AP Research 12
-# Social Justice 12
-# Spanish 12 or AP Spanish Language & Culture 12 or DELE Level B2
-# AP Studio Art 12
-# AP United States History 12
-# Writing 12
+
+
+grade10_courses = ("English 10",
+"Foundations of Mathematics", "Pre-Calculus 10",
+"Biology", "Chemistry", "Physics", "Earth and Space Science",
+"Social Studies 10"
+)
+
+grade10_electives = ("Coding 11: Javascript",
+"Coding 11: Python",
+"Design 11",
+"Introduction to Business 11",
+"Science & Ethics 11",
+"AP Seminar 11",
+)
+
+print(*grade10_electives, sep = "\n") #prints them all in new lines
---
+++ Program.py
@@ -0,0 +1,22 @@
+#ASSIGNING_ALL_SCHOOL_CLASSES_100_POINTS
+
+points = 100
+
+Pre_Calculus_11 = points
+Foundations_of_Mathematics_12 = points
+Twentyth_Century_World_History_12 = points
+BC_First_Peoples_12 = points
+Comparative_Cultures_12 = points
+Human_Geography_12 = points
+Law_Studies_12 = points
+Physical_Geography_12 = points
+Political_Studies_12 = points
+Social_Justice_12 = points
+Chemistry_11_or_AP_Chemistry_11 = points
+Life_Sciences_11 = points
+AP_Biology_11 = points
+Geology_12 = points
+Physics_11 = points
+AP_Physics_11 = points
+
+print(Chemistry_11_or_AP_Chemistry_11 + 10)
